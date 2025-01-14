4ec52ebf638155bd4b5620bb17a30fb6eb62fe40
Muhammad Rafi Dhiyaulhaq <rafidhiyaulh@gmail.com>
2025-01-14 08:16:33 +0700

    app.config.js lupa diupdate env

--- app.config.js
+++ app.config.js
@@ -13,7 +13,7 @@ export default {
       },
       android: {
         package: "com.relativitylab.app",
-        googleServicesFile: "./android/app/google-services.json",
+        googleServicesFile: process.env.GOOGLE_SERVICES_JSON || "./android/app/google-services.json",
         adaptiveIcon: {
           foregroundImage: "./assets/adaptive-icon.png",
           backgroundColor: "#000000"
