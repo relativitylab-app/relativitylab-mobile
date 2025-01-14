e99f164dad76be00e95c0d3e8c5e3d6c710c36cc
Muhammad Rafi Dhiyaulhaq <rafidhiyaulh@gmail.com>
2025-01-14 08:11:42 +0700

    update config dan google service

---
+++ app.config.js
@@ -0,0 +1,36 @@
+export default {
+    expo: {
+      name: "RelativityLabMobileAndroid",
+      slug: "RelativityLabMobileAndroid",
+      version: "1.0.0",
+      orientation: "portrait",
+      icon: "./assets/icon.png",
+      userInterfaceStyle: "light",
+      splash: {
+        image: "./assets/splash-icon.png",
+        resizeMode: "contain",
+        backgroundColor: "#000000"
+      },
+      android: {
+        package: "com.relativitylab.app",
+        googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+        adaptiveIcon: {
+          foregroundImage: "./assets/adaptive-icon.png",
+          backgroundColor: "#000000"
+        }
+      },
+      ios: {
+        supportsTablet: true,
+        bundleIdentifier: "com.relativitylab.app"
+      },
+      plugins: [
+        "@react-native-firebase/app",
+        "@react-native-firebase/auth"
+      ],
+      extra: {
+        eas: {
+          projectId: "56e1814d-5745-4cc9-b248-f8433ef286d7"
+        }
+      }
+    }
+  }
